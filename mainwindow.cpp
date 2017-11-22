9365b365d4d44258c89806f59b389754e65f8ea4
Faelwen <francis.john.mccarthy@googlemail.com>
2017-11-22 01:05:51 +0100

    Open FG module function, and open db file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include <QFileDialog>
 #include <QMessageBox>
 
+#include <QtXml>
+
+
 #include <JlCompress.h>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -27,9 +30,6 @@ void MainWindow::on_actionAbout_triggered()
 {
     QString about_text = "A simple Pathfinder race and class editor for Fantasy Grounds";
     QMessageBox::about(this, "About", about_text);
-
-    JlCompress::compressFile("C:/temp/test.zip", "C:/temp/test.txt");
-    JlCompress::extractDir("C:/temp/test.zip", "C:/temp/");
 }
 
 void MainWindow::on_actionOpen_triggered()
@@ -37,17 +37,62 @@ void MainWindow::on_actionOpen_triggered()
     current_file_path = QFileDialog::getOpenFileName(this, "Open file");
     QFile file(current_file_path);
     if(!file.open(QFile::ReadOnly | QFile::Text)) {
-            QMessageBox::warning(this, "..", "file not opened");
+            QMessageBox::warning(this, "Error", "File not opened");
             return;
     }
 
-    // TODO Add unzip module and open and parse of definition.xml
+    QStringList module_content = JlCompress::getFileList(current_file_path);
+
+    if (module_content.isEmpty()) {
+        QMessageBox::warning(this, "Error", "File doesn't appear to be a Fantasy Grounds module.");
+        return;
+    }
+
+    bool contains_definition = false;
+    foreach(QString item, module_content) {
+        if (item == "db.xml") {
+            contains_definition = true;
+        }
+    }
+    if (!contains_definition) {
+        QMessageBox::warning(this, "Error", "Module doesn't contain db.xml file.");
+        return;
+    }
+
+    QStringList files_extracted = JlCompress::extractDir(current_file_path, QDir::currentPath());
+    QFile definition_xml ("db.xml");
+    if (!definition_xml.open(QIODevice::ReadOnly)) {
+        QMessageBox::warning(this, "Error", "Cannot open db.xml file.");
+        return;
+    }
+
+    QDomDocument *dom = new QDomDocument("db");
+    if(!dom->setContent(&definition_xml)) {
+        definition_xml.close();
+        QMessageBox::warning(this, "Error", "Cannot parse db.xml file.");
+        return;
+    }
+
+    QDomElement dom_element = dom->documentElement();
+    QDomNode root = dom_element.firstChild();
+    QDomNode node = root;
+
+    // TODO parse xml and insert results in mainwindow
+    while(!node.isNull()) {
+       QDomElement element = node.toElement();
+       if(!element.isNull())
+       {
+
+           node = node.nextSibling();
+       }
+
+    }
 }
 
 void MainWindow::on_actionSave_triggered()
 {
     if (current_file_path == "") {
-        QMessageBox::warning(this, "..", "No module currently opened");
+        QMessageBox::warning(this, "Error", "No module currently opened");
         return;
     }
     // TODO Generate new definition.xml and zip with module
@@ -58,7 +103,7 @@ void MainWindow::on_actionSave_as_triggered()
     current_file_path = QFileDialog::getSaveFileName(this, "Open file");
     QFile file(current_file_path);
     if(!file.open(QFile::WriteOnly | QFile::Text)) {
-            QMessageBox::warning(this, "..", "file not opened");
+            QMessageBox::warning(this, "Error", "file not opened");
             return;
     }
     // TODO Generate new definition.xml and zip with module
